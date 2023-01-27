7384c8ca852e879fe043da7b24e70e68a3f003f0
Bhavana Shrotri <Bhavana.Shrotri@Klingelnberg.com>
2023-01-27 15:38:25 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using EventStore.Client;
+using System.Text;
+
+var client = new EventStorePersistentSubscriptionsClient(EventStoreClientSettings.Create("esdb://localhost:2113?tls=false"));
+
+await client.CreateAsync("HelloWorld", "TestGroup", new PersistentSubscriptionSettings(startFrom: StreamPosition.End));
+
+await SubscribeAsync(client);
+
+static Task<PersistentSubscription> SubscribeAsync(EventStorePersistentSubscriptionsClient client)
+{
+    return client.SubscribeToStreamAsync("HelloWorld", "TestGroup",
+        (subscription, evt, retryCount, cancelToken) =>
+        {
+            Console.WriteLine("Received: " + Encoding.UTF8.GetString(evt.Event.Data.Span));
+
+            return Task.CompletedTask;
+        });
+}
+
+Console.ReadLine();
