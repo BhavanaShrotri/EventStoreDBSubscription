683cc0b279f2834ea9e6e09ec38881d064513e43
Bhavana Shrotri <Bhavana.Shrotri@Klingelnberg.com>
2023-01-30 12:19:12 +0530

    Acknowleged the event

--- Program.cs
+++ Program.cs
@@ -6,18 +6,20 @@ using System.Text;
 
 var client = new EventStorePersistentSubscriptionsClient(EventStoreClientSettings.Create("esdb://localhost:2113?tls=false"));
 
-await client.CreateAsync("HelloWorld", "TestGroup", new PersistentSubscriptionSettings(startFrom: StreamPosition.End));
+// await client.CreateAsync("HelloWorld", "TestGroup", new PersistentSubscriptionSettings(startFrom: StreamPosition.End));
 
 await SubscribeAsync(client);
 
 static Task<PersistentSubscription> SubscribeAsync(EventStorePersistentSubscriptionsClient client)
 {
     return client.SubscribeToStreamAsync("HelloWorld", "TestGroup",
-        (subscription, evt, retryCount, cancelToken) =>
+        async (subscription, evt, retryCount, cancelToken) =>
         {
+           // await HandleEvent(evnt);
+            await subscription.Ack(evt);
             Console.WriteLine("Received: " + Encoding.UTF8.GetString(evt.Event.Data.Span));
 
-            return Task.CompletedTask;
+            //return Task.CompletedTask;
         });
 }
 
